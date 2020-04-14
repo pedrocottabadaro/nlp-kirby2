0cc837a63d6eeb6b47602d56021611ece6c49f45
pedrocotta98 <37852363+pedrocotta98@users.noreply.github.com>
2020-04-14 03:55:31 -0300

    falta and

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
-segundo=False
-palavrasArquivo = []
-listaTabela=[]
-listaTabelaValor=[]
 
-palavras=""
-palavrasAtributo=""
-contadorReferenciaAtributo=-1
-vetorClasse=[]
-vetorReferenciaMetodoClasse=[]
-vetorReferenciaAtributoMetodo=[]
-vetorMetodo=[]
-vetorAtributo=[]
-vetorValorAtributo=[]
-vetorSplit=[]
-vetorSplitAtributo=[]
-vetorReferenciaValorAtributo=[]
-entrouIfGiven=False
-examples = False
-given=False
-when=False
-then=False
-posSeta=False
-primeiro=False
-contador=0
-i = 1
-seta=False
-palavrasSeta=[]
+nlp = spacy.load('en_core_web_sm')
+
+palavrasArquivo = []
+
+vetorfrase=[]
+
+vetorReferenciaMetodoClasse = []
+vetorReferenciaAtributoClasse = []
+vetorReferenciaParametroMetodo=[]
+vetorReferenciaValorAtributo = []
+
+
+vetorGiven=[]
+vetorWhen=[]
+vetorThen=[]
+vetorAnd=[]
+vetorParametro=[]
+
+vetorClasse = []
+vetorMetodo = []
+vetorAtributo = []
+vetorValorAtributo = []
+
+
+
+
+
 arquivo = open('caso01.txt', 'r')
-contadorMetodo=0
-contadorAtributo=0
+contadorClasse=0
+contadorMetodo = 0
+contadorParametro=0
+contadorAtributo = 0
+
+ran=False
+
+b=[]
 for linha in arquivo:
     linha = linha.strip()
     palavrasArquivo.append(linha)
@@ -39,225 +45,102 @@ arquivo.close
 for words in palavrasArquivo:
     doc = nlp(words)
 
-    ##pegar palavras
-    if "Given" in words:
-        given=True
-        when=False
-        examples=False
-        then=False
-      
-    if "When" in words:
-        given = False
-        when=True
-        examples=False
-        then=False
-        for palavra in vetorSplit:
-            palavras = palavras + palavra
-
-        vetorClasse.append(palavras)
-
-        for palavra in vetorSplitAtributo:
-            palavrasAtributo = palavrasAtributo + palavra
-
-        if len(palavrasAtributo) > 3:
-             vetorAtributo.append(palavrasAtributo)
-        contador=0
-        palavras=""
-        palavrasAtributo=""
-        primeiro=False
-        segundo=False
-        vetorSplit=[]
-        vetorSplitAtributo=[]
-
-    if "Then" in words:
-        then=True
-        when=False
-        examples=False
-        for palavra in vetorSplit:
-            palavras = palavras + palavra
-
-        vetorMetodo.append(palavras)
-
-        for palavra in vetorSplitAtributo:
-            palavrasAtributo = palavrasAtributo + palavra
-
-        if len(palavrasAtributo) > 1:
-            vetorAtributo.append(palavrasAtributo)
-        primeiro=False
-        segundo=False
-        contador=0
-        vetorSplit = []
-        vetorSplitAtributo = []
-        palavras=""
-        palavrasAtributo=""
-
-    if "Examples" in words:
-        contadorReferenciaAtributo = contadorReferenciaAtributo + 1
-        contadorAtributo = contadorAtributo + 1
-        contadorMetodo = contadorMetodo + 1
-        examples = True
-        then=False
-        given=False
-        when=False
-        for palavra in vetorSplitAtributo:
-            palavrasAtributo = palavrasAtributo + palavra
-
-        if len(palavrasAtributo) > 1:
-            vetorAtributo.append(palavrasAtributo)
-    if given:
-        for token in doc:
-
-            if contador==0:
-
-                if token.text=='"' and not primeiro:
-                    primeiro=True
-                elif token.text == '"':
-                    primeiro=False
-                    contador=contador+1
-                    continue
-                elif primeiro:
-                    vetorSplit.append(token.text)
-
-
-
-            if not primeiro:
-                if token.text == '"' and not segundo:
-                    segundo = True
-
-                elif token.text == '"':
-                    segundo=False
-                    continue
-                elif segundo:
-                    vetorSplitAtributo.append(token.text)
+    if words.split(" ").__contains__("Given") or words.split(" ").__contains__("Then") or words.split(" ").__contains__("When") or words.split(" ").__contains__("And"):
+        vetorfrase.append(words)
 
 
 
 
 
-            """
-            if token.text=="<": 
-                seta=True
-            elif seta:
-                if token.text==">":
-                    seta=False
-                else:
-                    posSeta=True
-                    palavrasSeta.append(token.text)
-            elif posSeta:
-                vetorAtributo.append(token.text)
-                if not entrouIfGiven:
-                    vetorClasse.append(token.text)
+for x in range(len(vetorfrase)):
 
-            elif token.pos_ != 'DET' and token.is_stop == False and token.pos_ != "ADJ" and \
-                    token.pos != "PRON" and token.text != "|" and token.text != "" and token.text != ":" and token.text != "Given" and token.text != " " :
-                if not(vetorClasse.__contains__(token.text)):
-                    entrouIfGiven=True
-                    vetorClasse.append(token.text)
-                    """
+    if(vetorfrase[x].__contains__("Given")):
+        vetorGiven.append(vetorfrase[x])
+    elif (vetorfrase[x].__contains__("When")):
+        vetorWhen.append(vetorfrase[x])
+    elif (vetorfrase[x].__contains__("Then")):
+        vetorThen.append(vetorfrase[x])
+    elif (vetorfrase[x].__contains__("And")):
+        vetorAnd.append(vetorfrase[x])
 
 
-    posSeta=False
-    if when:
-        for token in doc:
 
-            if contador == 0:
-
-                if token.text == '"' and not primeiro:
-                    primeiro = True
-                elif token.text == '"':
-                    primeiro = False
-                    contador = contador + 1
-                    continue
-                elif primeiro:
-                    vetorSplit.append(token.text)
-
-            if not primeiro:
-                if token.text == '"' and not segundo:
-                    segundo = True
-
-                elif token.text == '"':
-                    segundo = False
-                    continue
-                elif segundo:
-                    vetorSplitAtributo.append(token.text)
-
-            """
-            if token.text=="<":
-                seta=True
-            elif seta:
-                if token.text==">":
-                    seta=False
-                else:
-                    posSeta=True
-                    palavrasSeta.append(token.text)
-            elif posSeta:
-                if not vetorAtributo.__contains__(token.text):
-                    vetorAtributo.append(token.text)
-            elif token.pos_ != 'DET' and token.is_stop == False and token.pos_ != "ADJ" and \
-                    token.pos != "PRON" and token.text != "|" and token.text != "" and token.text != ":" and token.text != "Given" :
-                vetorMetodo.append(token.text)
-                vetorReferenciaMetodoClasse.append(contadorMetodo)
-                """
-
-    if then:
-        for token in doc:
+possuiAtributo=False
+ran=False
+valor=""
+metadeAtributo=""
+possuiAtributo=False
+percorreu=False
+possuiClasse=False
+for x in range(len(vetorGiven)):
+    vetaux=vetorGiven[x].split(" ")
 
 
-           if contador == 0:
+    for y in vetaux:
 
-            if token.text == '"' and not primeiro:
-                primeiro = True
-            elif token.text == '"':
-                primeiro = False
-                contador = contador + 1
+        if y.__contains__('"'):
+            b = y.split('"')
+            if vetorClasse.__contains__(b[1]):
+                possuiClasse=True
                 continue
-            elif primeiro:
-                vetorSplitAtributo.append(token.text)
+            elif not possuiClasse and (not ran )and (not percorreu):
+                vetorClasse.append(b[1])
+                ran=True
+            percorreu=True
 
+            if b[1].isdecimal() or b[1] == "True" or b[1] == "False" and percorreu and possuiClasse and  (not vetorClasse.__contains__(b[1])):
+                valor=b[1]
+            elif percorreu and (not vetorClasse.__contains__(b[1])) :
+                possuiAtributo=True
+                vetorAtributo.append(b[1])
+                vetorReferenciaAtributoClasse.append(contadorClasse)
+                contadorAtributo+=1
 
-            """"
-            if token.text=="<":
-                seta=True
-            elif seta:
-                if token.text==">":
-                    seta=False
-                else:
-                    palavrasSeta.append(token.text)
-            elif token.pos_ != 'DET' and token.is_stop == False and token.pos_ != "ADJ" and \
-                    token.pos != "PRON" and token.text != "|" and token.text != "" and token.text != ":" and token.text != "Given" :
-                vetorReferenciaAtributoMetodo.append(contadorAtributo)
-                if not vetorAtributo.__contains__(token.text):
-                        vetorAtributo.append(token.text)
-                """
+    if valor!="":
+        if not possuiAtributo:
+            vetorAtributo.append(vetorClasse[contadorClasse])
+            vetorReferenciaAtributoClasse.append(contadorClasse)
+            contadorAtributo+=1
+        vetorValorAtributo.append(valor)
+        vetorReferenciaValorAtributo.append(contadorAtributo-1)
 
+    ran=False
+    valor=""
+    vetaux=vetorWhen[x].split()
+    possuiAtributo=False
+    for y in vetaux:
+        if y.__contains__('"'):
+            b=y.split('"')
+            if not ran:
+                vetorMetodo.append(b[1])
+                vetorReferenciaMetodoClasse.append(contadorClasse)
+                ran=True
 
+            else:
+                vetorParametro.append(b[1])
+                vetorReferenciaParametroMetodo.append(contadorMetodo)
+                contadorMetodo+=1
+                contadorParametro += 1
 
+    ran = False
+    valor = ""
+    vetaux = vetorThen[x].split()
+    possuiAtributo = False
+    for y in vetaux:
+        if y.__contains__('"'):
+            b=y.split('"')
 
-    ##pegar dados tabela
+            vetorAtributo.append(vetorAtributo[contadorAtributo-1])
+            vetorReferenciaAtributoClasse.append(contadorClasse)
+            contadorAtributo+=1
+            vetorValorAtributo.append(b[1])
 
-
-
-    if examples:
-        if i < 2:
-            if not ("Examples" in words):
-                for token in doc:
-                    if token.text != "|" and token.text != "" and token.text != ":" and token.text != "Examples":
-                        listaTabela.append(token.text)
-
-
-                i = i + 1
-        else:
-            for token in doc:
-                if token.pos_ == "NUM" and token.text != "|":
-                    listaTabelaValor.append(token.text)
-                    vetorValorAtributo.append(token.text)
-                    vetorReferenciaValorAtributo.append(contadorReferenciaAtributo)
-
-            contadorReferenciaAtributo+1
-
-
-
-
+    ran = False
+    valor = ""
+    possuiAtributo = False
+    contadorClasse+=1
+    possuiClasse=False
+    percorreu=False
 
 
 print("classe:")
@@ -266,14 +149,16 @@ print("metodo")
 print(vetorMetodo)
 print("referencia metodo a classe")
 print(vetorReferenciaMetodoClasse)
+print("Parametros")
+print(vetorParametro)
+print("referencia parametros")
+print(vetorReferenciaParametroMetodo)
 print("atributo")
 print(vetorAtributo)
-print("referencia atributo ao metodo")
-print(vetorReferenciaAtributoMetodo)
+print("referencia atributo a Classe")
+print(vetorReferenciaAtributoClasse)
 print("valor atributo")
 print(vetorValorAtributo)
-print("referencia valor ao atributo")
-print(vetorReferenciaValorAtributo)
 
 
 
